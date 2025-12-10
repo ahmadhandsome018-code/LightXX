8ca1dfd2f22012ff301103ef024c2ceb2cfdf1dc
ahmadhandsome018-code <ahmadhandsome018@gmail.com>
2025-12-10 16:23:43 +0200

    Update SmsForwardService.java

--- app/src/main/java/com/lightx/smsforwarder/SmsForwardService.java
+++ app/src/main/java/com/lightx/smsforwarder/SmsForwardService.java
@@ -1,14 +1,24 @@
-sb.append("{");
-sb.append("\"sender\": {\"name\": \"LightX SMS Forwarder\", \"email\": \"")
-        .append(escapeJson("alahlymasr9@gmail.com"))
-        .append("\"},");
-sb.append("\"to\": [{\"email\": \"")
-        .append(escapeJson("ainsports12@gmail.com"))
-        .append("\"}],");
-sb.append("\"subject\": \"")
-        .append(escapeJson(subject))
-        .append("\",");
-sb.append("\"htmlContent\": \"")
-        .append(escapeJson(content))
-        .append("\"");
-sb.append("}");
+private String buildEmailJson(String subject, String content) {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("{");
+    sb.append("\"sender\": {\"name\": \"LightX SMS Forwarder\", \"email\": \"")
+            .append(escapeJson("alahlymasr9@gmail.com"))
+            .append("\"},");
+
+    sb.append("\"to\": [{\"email\": \"")
+            .append(escapeJson("ainsports12@gmail.com"))
+            .append("\"}],");
+
+    sb.append("\"subject\": \"")
+            .append(escapeJson(subject))
+            .append("\",");
+
+    sb.append("\"htmlContent\": \"")
+            .append(escapeJson(content))
+            .append("\"");
+
+    sb.append("}");
+
+    return sb.toString();
+}
